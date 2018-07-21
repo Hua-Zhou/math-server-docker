c006cb1c01172a0c7afaff87e25db0b9047060df
Hua Zhou <huazhou@ucla.edu>
2018-07-20 22:44:55 -0700

    add unregistered packages from OpenMendel

--- libs/libs_julia.jl
+++ libs/libs_julia.jl
@@ -5,3 +5,9 @@ for l in libs
     info("Adding package $l...")
     Pkg.add(l)
 end
+
+Pkg.clone("https://github.com/OpenMendel/MendelBase.jl.git")
+Pkg.clone("https://github.com/OpenMendel/MendelGWAS.jl.git")
+Pkg.clone("https://github.com/OpenMendel/MendelKinship.jl.git")
+Pkg.clone("https://github.com/OpenMendel/SnpArrays.jl.git")
+Pkg.clone("https://github.com/OpenMendel/VarianceComponentModels.jl.git")
